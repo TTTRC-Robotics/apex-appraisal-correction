6be0b7caa6c7df19eef65e8bdd34a1a1f4a715b7
Florent Chretien <flopubs@gmail.com>
2023-10-13 15:53:39 +0600

    add function do_nothing

--- exercice_2/exercice_2.ino
+++ exercice_2/exercice_2.ino
@@ -7,3 +7,10 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+void do_nothing(){
+  // this is an empty function that do nothing and is named do_nothing
+  // this has no lines of code, only comments
+  // return a void
+
+}
